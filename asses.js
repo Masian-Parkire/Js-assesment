f4984f8115065c016d834a7558d6e13f2e3efc84
Masian Parkire <parkiremasian@gmail.com>
2023-04-11 09:03:34 +0300

    made changes

--- asses.js
+++ asses.js
@@ -1,19 +1,22 @@
 //Write a function that takes in a string and returns it when reversed
 // let food = “eating”
-// let food = ("eating")
-// let a = food.reverse()
-// console.log(a)
+let food = ("eating");
+let a = food.reverse();
+console.log(a);
 
 //Write a function that takes in the following array and consoles the target if it is found else
 //null
-let num = [2,8,0,23,5,45,76]
+let num = [2,8,0,23,5,45,76];
 Target = 23
 let i = 0
 for (let i = 0; i < num.length; i++) {
     const element = num[i];
-    if (num[i]) {
-        
+    if (element[1]===23) {
+        console.log(element)
     }
+    else{
+        return null
+    };
     
 }
 
@@ -25,13 +28,24 @@ for (let i = 0; i < num.length; i++) {
 
 //Given a range of numbers from 0 to 100, console”Fizz” if the numbers are divisible by 3,
 //“Buzz” if the numbers are divisible by 5, and “FizzBuzz” if divisible by both 3 and 5.
-
+let numbers =[1,...100]
+numbers.forEach(value => {
+    if (value%3===0 && value %5 ===0){
+        console.log("FizzBuzz")
+    }
+    else if(value%3===0){
+        console.log("Fizz")
+    }
+    else{
+        console.log("Buzz")
+    }
+});
 
 //Write a function that takes in an array of numbers and returns an array that has all
 //elements multiplied by 4.
 //let numArray = [12,87,45,75,23,64,73]
 
-let numArray = [12,87,45,75,23,64,73]
+let numArray = [12,87,45,75,23,64,73];
 numArray.forEach(element => {
     console.log(element*4)
 });
